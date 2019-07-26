e9dec0f2492130ab2e01f6a0147d57229b960bcc
yukiai0227 <50581068+yukiai0227@users.noreply.github.com>
2019-07-26 16:39:23 +0900

    Update kadai10.m

--- kadai10.m
+++ kadai10.m
@@ -3,7 +3,7 @@
 % 各自，Lenna以外の画像を用いよ． 
 % 例
 
-ORG = imread('Lenna.jpg'); % 原画像の入力
+ORG = imread('kadai10.jpg'); % 原画像の入力
 ORG = rgb2gray(ORG); %カラーからグレイへの変換
 imagesc(ORG); colormap('gray'); colorbar;% 画像表示
 pause; % 一時停止
@@ -19,6 +19,3 @@ pause; % 一時停止
 IMG = edge(ORG,'canny'); % エッジ抽出（キャニー法）
 imagesc(IMG); colormap('gray'); colorbar;% 画像表示
 pause; % 一時停止
-
-
-
